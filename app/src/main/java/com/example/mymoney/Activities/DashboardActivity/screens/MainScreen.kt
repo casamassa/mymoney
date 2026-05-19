85567d9692dc934a058c8a88ce404582315c063b
Marcelo Casamassa <marcelocasamassa@gmail.com>
2026-05-19 09:17:46 -0300

    Action Button Row

--- app/src/main/java/com/example/mymoney/Activities/DashboardActivity/screens/MainScreen.kt
+++ app/src/main/java/com/example/mymoney/Activities/DashboardActivity/screens/MainScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.mymoney.Activities.DashboardActivity.components.ActionButtonRow
 import com.example.mymoney.Activities.DashboardActivity.components.CardSection
 import com.example.mymoney.Activities.DashboardActivity.components.HeaderSection
 import com.example.mymoney.Domain.ExpenseDomain
@@ -44,6 +45,7 @@ fun MainScreen(
         ) {
             item { HeaderSection() }
             item { CardSection (onCardClick) }
+            item { ActionButtonRow() }
         }
     }
 }

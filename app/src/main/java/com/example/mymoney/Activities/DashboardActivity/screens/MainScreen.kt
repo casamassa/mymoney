469b1fdff2a54b65c7fb6f81020aa8f217dfe1fa
Marcelo Casamassa <marcelocasamassa@gmail.com>
2026-05-19 08:43:11 -0300

    Card Section

--- app/src/main/java/com/example/mymoney/Activities/DashboardActivity/screens/MainScreen.kt
+++ app/src/main/java/com/example/mymoney/Activities/DashboardActivity/screens/MainScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.mymoney.Activities.DashboardActivity.components.CardSection
 import com.example.mymoney.Activities.DashboardActivity.components.HeaderSection
 import com.example.mymoney.Domain.ExpenseDomain
 
@@ -42,6 +43,7 @@ fun MainScreen(
             verticalArrangement = Arrangement.spacedBy(space=12.dp)
         ) {
             item { HeaderSection() }
+            item { CardSection (onCardClick) }
         }
     }
 }

d4f2e2e19d197a238a28e79d8a832cc819be13ad
Marcelo Casamassa <marcelocasamassa@gmail.com>
2026-05-19 15:15:47 -0300

    Summary Columns

--- app/src/main/java/com/example/mymoney/Activities/ReportActivity/screens/ReportScreen.kt
+++ app/src/main/java/com/example/mymoney/Activities/ReportActivity/screens/ReportScreen.kt
@@ -7,15 +7,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.mymoney.Activities.ReportActivity.components.CenterStatsCard
 import com.example.mymoney.Activities.ReportActivity.components.GradientHeader
+import com.example.mymoney.Activities.ReportActivity.components.SummaryColumns
 import com.example.mymoney.Domain.BudgetDomain
+import com.example.mymoney.R
 
 @Composable
 fun ReportScreen(
@@ -81,6 +85,19 @@ fun ReportContent(
                         }
                 )
             }
+        }
+
+        item {
+            SummaryColumns(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp)
+                    .background(
+                        color = colorResource(R.color.lightBlue),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                    .padding(8.dp)
+            )
         }
     }
 }
